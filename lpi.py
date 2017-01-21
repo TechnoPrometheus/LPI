9cddba4f82f8801a2beb0f03e099b26d0fb55641
TechnoPrometheus <technoprometheus@gmail.com>
2017-01-21 14:22:53 +0100

    fixed menu

--- lpi.py
+++ lpi.py
@@ -17,7 +17,7 @@
 
 #===============================================================================
 #title           :lpi.py
-#description     :Linux Post-Installer script.
+#description     :Linux Post-Installer script
 #author          :Stefano Peris
 #email           :technoprometheus@gmail.com
 #date            :21/01/2017
@@ -39,42 +39,67 @@
 # Librerie e moduli
 import os
 
-# Variabili globali
-opzione = 0
-
 # Pulisce lo schermo
 def clear_screen():
     os.system('clear')
 
 #===============================================================================
 
-while opzione:
-    print("""
-    *---------------------------------*
-    * LINUX POST INSTALLER - Ver. 0.1 *
-    *---------------------------------*
+def menu():
+    print("*---------------------------------*")
+    print("* LINUX POST INSTALLER - Ver. 0.1 *")
+    print("*---------------------------------*")
+    print("")
+    print("+------------------ ABOUT -------------------+")
+    print("+ Autore: Stefano Peris <TechnoPrometheus>   +")
+    print("+ eMail: technoprometheus@gmail.com          +")
+    print("+--------------------------------------------+")
+    print("")
+    print("+------------------------+")
+    print("+--------- MENU ---------+")
+    print("+------------------------+")
+    print("")
+    print("")
+    print("[1] Programmi CLI (Terminale)")
+    print("[2] Utility")
+    print("[3] Compilatori C/C++")
+    print("[4] Librerie C/C++")
+    print("[5] Tool di sviluppo")
+    print("[6] Ambienti di sviluppo (IDE)")
+    print("[7] Programmi di grafica 2D/3D")
+    print("[8] Giochi")
+    print("[i] Info")
+    print("[x] Esci")
 
-    +------------------ ABOUT -------------------+
-    + Autore: Stefano Peris <TechnoPrometheus>   +
-    + eMail: technoprometheus@gmail.com          +
-    +--------------------------------------------+
+loop = True
 
-    +------------------------+
-    +--------- MENU ---------+
-    +------------------------+
-
-
-    [1] Programmi CLI (Terminale)
-    [2] Utility
-    [3] Compilatori C/C++
-    [4] Librerie C/C++
-    [5] Tool di sviluppo
-    [6] Ambienti di sviluppo (IDE)
-    [7] Programmi di grafica 2D/3D
-    [8] Giochi
-    [i] Info
-    [x] Esci
-    """)
-
+while loop:        ## Ciclo che prosegue fino a quando loop = False
+    
+    menu()         ## Visualizza il menu nel terminale
+    print("\n")
+    print("\n")
+    print("\n")
+    
     opzione = input("Digita l'opzione desiderata > ")
+     
+    if opzione == 1:     
+        print ("Menu 1 è stato selezionato")
+        ## È possibile aggiungere il codice o funzioni qui
+    elif opzione == 2:
+        print ("Menu 2 è stato selezionato")
+        ## È possibile aggiungere il codice o funzioni qui
+    elif opzione == 3:
+        print ("Menu 3 è stato selezionato")
+        ## È possibile aggiungere il codice o funzioni qui
+    elif opzione == 4:
+        print ("Menu 4 è stato selezionato")
+        ## È possibile aggiungere il codice o funzioni qui
+    elif opzione == 5:
+        print ("Menu 5 è stato selezionato")
+        ## È possibile aggiungere il codice o funzioni qui
+        loop = False # Ferma il ciclo
+    else:
+        # Valori diversi da 1-8 e i-x generano un messaggio di errore
+        input("Errore. Premi un tasto per riprovare...")
+        
 
